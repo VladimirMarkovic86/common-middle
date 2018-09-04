4bd0c9c6a57d2ed5e9734c74ef77d0ae4f717437
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-04 18:02:20 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject org.vladimir/common-middle "0.1.0"
+  :description    "Common middleware"
+  :url            "http://gitlab:1610/VladimirMarkovic86/common-middle"
+  :license        {:name "Eclipse Public License"
+                   :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies   [[org.clojure/clojure	"1.9.0"]
+                   [org.clojure/clojurescript "1.10.339"]
+                   ]
+  :source-paths ["src/cljc"])
+
