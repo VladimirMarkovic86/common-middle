2e7a13b291f475201712a4a5194a3c95e2436a81
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:44:18 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject org.vladimir/common-middle "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/common-middle "0.1.0"
   :description "Common middleware"
-  :url "http://gitlab:1610/VladimirMarkovic86/common-middle"
+  :url "http://github.com/VladimirMarkovic86/common-middle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljc"])
 
